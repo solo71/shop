6115d8dd54414bbeae3ce1e6e94c0a1201db3fea
solo7 <solo77.job@gmail.com>
2017-10-15 20:15:02 +0300

    new

--- vagrant/provision/always-as-root.sh
+++ vagrant/provision/always-as-root.sh
@@ -13,6 +13,9 @@ function info {
 info "Provision-script user: `whoami`"
 
 info "Restart web-stack"
-service php7.0-fpm restart
+service php7.1-fpm restart
 service nginx restart
 service mysql restart
+service elasticsearch restart
+service redis restart
+service supervisor restart
--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -26,12 +26,38 @@ debconf-set-selections <<< "mysql-community-server mysql-community-server/root-p
 debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password \"''\""
 echo "Done!"
 
+info "Add PHp 7.1 repository"
+add-apt-repository ppa:ondrej/php -y
+
+info "Add Oracle JDK repository"
+add-apt-repository ppa:webupd8team/java -y
+
+info "Add ElasticSearch sources"
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
 info "Update OS software"
 apt-get update
 apt-get upgrade -y
 
 info "Install additional software"
-apt-get install -y php7.0-curl php7.0-cli php7.0-intl php7.0-mysqlnd php7.0-gd php7.0-fpm php7.0-mbstring php7.0-xml unzip nginx mysql-server-5.7
+apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml php7.1-zip unzip nginx mysql-server-5.7
+
+info "Install Oracle JDK"
+debconf-set-selections <<< "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true"
+debconf-set-selections <<< "oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true"
+apt-get install -y oracle-java8-installer
+
+info "Install ElasticSearch"
+apt-get install -y elasticsearch
+sed -i 's/-Xms2g/-Xms64m/' /etc/elasticsearch/jvm.options
+sed -i 's/-Xmx2g/-Xmx64m/' /etc/elasticsearch/jvm.options
+
+info "Install Redis"
+apt-get install -y redis-server
+
+info "Install Supervisor"
+apt-get install -y supervisor
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -42,9 +68,9 @@ mysql -uroot <<< "FLUSH PRIVILEGES"
 echo "Done!"
 
 info "Configure PHP-FPM"
-sed -i 's/user = www-data/user = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/group = www-data/group = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
+sed -i 's/user = www-data/user = vagrant/g' /etc/php/7.1/fpm/pool.d/www.conf
+sed -i 's/group = www-data/group = vagrant/g' /etc/php/7.1/fpm/pool.d/www.conf
+sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/7.1/fpm/pool.d/www.conf
 echo "Done!"
 
 info "Configure NGINX"
@@ -56,8 +82,12 @@ ln -s /app/vagrant/nginx/app.conf /etc/nginx/sites-enabled/app.conf
 echo "Done!"
 
 info "Initailize databases for MySQL"
-mysql -uroot <<< "CREATE DATABASE yii2advanced"
-mysql -uroot <<< "CREATE DATABASE yii2advanced_test"
+mysql -uroot <<< "CREATE DATABASE shop"
+mysql -uroot <<< "CREATE DATABASE shop_test"
+echo "Done!"
+
+info "Enabling supervisor processes"
+ln -s /app/vagrant/supervisor/queue.conf /etc/supervisor/conf.d/queue.conf
 echo "Done!"
 
 info "Install composer"
